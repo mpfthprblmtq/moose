13fed63f2eed268fc703962c43b8c4a5713d9c8a
Pat Ripley <pripley@siue.edu>
2018-03-12 07:42:35 -0500

    restructured repo, working on getting the right id3 tags to change

--- src/moose/Frame.java
+++ src/moose/Frame.java
@@ -2,18 +2,16 @@ package moose;
 
 import com.mpatric.mp3agic.InvalidDataException;
 import com.mpatric.mp3agic.Mp3File;
+import com.mpatric.mp3agic.NotSupportedException;
 import com.mpatric.mp3agic.UnsupportedTagException;
-import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.io.File;
 import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JMenuItem;
 import javax.swing.JPopupMenu;
@@ -21,10 +19,25 @@ import javax.swing.table.DefaultTableModel;
 
 public class Frame extends javax.swing.JFrame {
 
+    ArrayList<Song> songs = new ArrayList<>();
+    ArrayList<Song> edited_songs = new ArrayList<>();
+    
+    DefaultTableModel model;
+    ActionListener menuListener;
+    
     /**
      * Creates new form Frame
      */
     public Frame() {
+        this.menuListener = (ActionEvent event) -> {
+            if (event.getActionCommand().equals("Add")) {
+                
+            } else if (event.getActionCommand().equals("Remove")) {
+                
+            } else if (event.getActionCommand().equals("Remove from list")) {
+                model.removeRow(table.getSelectedRow());
+            }
+        };
         initComponents();
 
         // taken from the FileDrop example
@@ -33,9 +46,6 @@ public class Frame extends javax.swing.JFrame {
                 addFileToTable(file);
             }
         });
-        
-        String url="https://www.youtube.com/watch?v=iu1arvwNRxo";
-        String path="";
         
     }
 
@@ -55,7 +65,7 @@ public class Frame extends javax.swing.JFrame {
             Logger.getLogger(Frame.class.getName()).log(Level.SEVERE, null, ex);
         }
         
-        String filename = file.getName();
+        // get the id3v2 info
         String title = mp3file.getId3v2Tag().getTitle();
         String artist = mp3file.getId3v2Tag().getArtist();
         String album = mp3file.getId3v2Tag().getAlbum();
@@ -63,14 +73,33 @@ public class Frame extends javax.swing.JFrame {
         String genre = mp3file.getId3v2Tag().getGenreDescription();
         String track = mp3file.getId3v2Tag().getTrack();
         String disk = mp3file.getId3v2Tag().getPartOfSet();
-        ImageIcon artwork = new ImageIcon(mp3file.getId3v2Tag().getAlbumImage());
-        Image img = artwork.getImage();
-        Image img_scaled = img.getScaledInstance(100, 100, java.awt.Image.SCALE_SMOOTH);
-        Icon newIcon = new ImageIcon(img_scaled);
+        byte[] artwork_bytes = mp3file.getId3v2Tag().getAlbumImage();
         
-        DefaultTableModel model = (DefaultTableModel) table.getModel();
+        // create a song object
+        Song s = new Song(file, title, artist, album, albumartist, genre, track, disk, artwork_bytes);
         
-        model.addRow(new Object[]{filename, title, artist, album, albumartist, genre, track, disk, newIcon});
+        // add the song to the list
+        songs.add(s);
+        
+        // get the table model from the table
+        model = (DefaultTableModel) table.getModel();
+        
+        //add the song to the table
+        model.addRow(new Object[]{
+            s.getFile().getAbsolutePath(),
+            s.getTitle(),
+            s.getArtist(),
+            s.getAlbum(),
+            s.getAlbumartist(),
+            s.getGenre(),
+            s.getFullTrackString(),
+            s.getFullDiskString(),
+            s.getArtwork()
+        });
+    }
+    
+    public void updateConsole(String s) {
+        console.append(s + "\n");
     }
 
     /**
@@ -84,12 +113,12 @@ public class Frame extends javax.swing.JFrame {
 
         container = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
-        jButton1 = new javax.swing.JButton();
+        saveButton = new javax.swing.JButton();
         tableSP = new javax.swing.JScrollPane();
         table = new javax.swing.JTable();
-        jScrollPane2 = new javax.swing.JScrollPane();
-        dragArea = new javax.swing.JTextArea();
-        jLabel2 = new javax.swing.JLabel();
+        consoleSP = new javax.swing.JScrollPane();
+        console = new javax.swing.JTextArea();
+        jButton1 = new javax.swing.JButton();
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
         jMenu2 = new javax.swing.JMenu();
@@ -100,7 +129,12 @@ public class Frame extends javax.swing.JFrame {
         jLabel1.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
         jLabel1.setText("Moose");
 
-        jButton1.setText("jButton1");
+        saveButton.setText("Save");
+        saveButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                saveButtonActionPerformed(evt);
+            }
+        });
 
         DefaultTableModel tableModel = new DefaultTableModel()
         {
@@ -108,6 +142,7 @@ public class Frame extends javax.swing.JFrame {
             public Class getColumnClass(int column)
             {
                 if (column == 8) return ImageIcon.class;
+                if (column == 0) return File.class;
                 return Object.class;
             }
         };
@@ -122,6 +157,7 @@ public class Frame extends javax.swing.JFrame {
         tableModel.addColumn("Track");
         tableModel.addColumn("Disk");
         tableModel.addColumn("Artwork");
+        table.setAutoCreateRowSorter(true);
         table.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
                 tableMouseClicked(evt);
@@ -129,12 +165,18 @@ public class Frame extends javax.swing.JFrame {
         });
         tableSP.setViewportView(table);
 
-        dragArea.setEditable(false);
-        dragArea.setColumns(20);
-        dragArea.setFont(new java.awt.Font("Monospaced", 0, 14)); // NOI18N
-        dragArea.setRows(5);
-        dragArea.setText("Drag files into me!");
-        jScrollPane2.setViewportView(dragArea);
+        console.setEditable(false);
+        console.setColumns(20);
+        console.setFont(new java.awt.Font("Monospaced", 0, 11)); // NOI18N
+        console.setRows(5);
+        consoleSP.setViewportView(console);
+
+        jButton1.setText("jButton1");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout containerLayout = new javax.swing.GroupLayout(container);
         container.setLayout(containerLayout);
@@ -143,18 +185,14 @@ public class Frame extends javax.swing.JFrame {
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, containerLayout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(containerLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(tableSP, javax.swing.GroupLayout.DEFAULT_SIZE, 1380, Short.MAX_VALUE)
-                    .addGroup(containerLayout.createSequentialGroup()
-                        .addGap(0, 0, Short.MAX_VALUE)
-                        .addComponent(jButton1))
+                    .addComponent(tableSP, javax.swing.GroupLayout.DEFAULT_SIZE, 1579, Short.MAX_VALUE)
+                    .addComponent(jLabel1, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addGroup(javax.swing.GroupLayout.Alignment.LEADING, containerLayout.createSequentialGroup()
+                        .addComponent(consoleSP, javax.swing.GroupLayout.PREFERRED_SIZE, 500, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addGroup(containerLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addGroup(containerLayout.createSequentialGroup()
-                                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 301, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 81, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                        .addGap(0, 0, Short.MAX_VALUE)))
+                            .addComponent(saveButton)
+                            .addComponent(jButton1))))
                 .addContainerGap())
         );
         containerLayout.setVerticalGroup(
@@ -166,10 +204,12 @@ public class Frame extends javax.swing.JFrame {
                 .addComponent(tableSP, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(containerLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 81, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jButton1)
+                    .addComponent(consoleSP, javax.swing.GroupLayout.DEFAULT_SIZE, 124, Short.MAX_VALUE)
+                    .addGroup(containerLayout.createSequentialGroup()
+                        .addComponent(saveButton)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jButton1)
+                        .addGap(0, 0, Short.MAX_VALUE)))
                 .addContainerGap())
         );
 
@@ -209,10 +249,11 @@ public class Frame extends javax.swing.JFrame {
             int row = table.rowAtPoint(evt.getPoint());
             int col = table.columnAtPoint(evt.getPoint());
             if (row >= 0 && col >= 0) {
-                System.out.println("row: " + row + ", col: " + col);
                 table.setRowSelectionInterval(row, row);
                 if (col == 8) {
-                    showPopup(evt);
+                    showArtworkPopup(evt);
+                } else {
+                    showRegularPopup(evt);
                 }
             } else {
                 table.clearSelection();
@@ -220,7 +261,53 @@ public class Frame extends javax.swing.JFrame {
         }
     }//GEN-LAST:event_tableMouseClicked
 
-    void showPopup(MouseEvent e) {
+    private void saveButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_saveButtonActionPerformed
+        for (int i = 0; i < model.getRowCount(); i++) {
+            
+            File file = (File) model.getValueAt(i, 0);
+            String title = model.getValueAt(i, 1).toString();
+            String artist = model.getValueAt(i, 2).toString();
+            String album = model.getValueAt(i, 3).toString();
+            String albumartist = model.getValueAt(i, 4).toString();
+            String genre = model.getValueAt(i, 5).toString();
+            String track = model.getValueAt(i, 6).toString();
+            String disk = model.getValueAt(i, 7).toString();
+            //byte[] artwork = model.getValueAt(i, 8);
+            
+            edited_songs.add(new Song(file, title, artist, album, albumartist, genre, track, disk, null));
+            
+        }
+        removeUnchanged();
+        
+        
+    }//GEN-LAST:event_saveButtonActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        
+        File file = new File(model.getValueAt(table.getSelectedRow(), 0).toString());
+        Mp3File mp3file = null;
+        try {
+            mp3file = new Mp3File(file.getAbsolutePath());
+            //System.out.println("Length of this mp3 is: " + mp3file.getLengthInSeconds() + " seconds");
+            //System.out.println("Bitrate: " + mp3file.getBitrate() + " kbps " + (mp3file.isVbr() ? "(VBR)" : "(CBR)"));
+            //System.out.println("Sample rate: " + mp3file.getSampleRate() + " Hz");
+            //System.out.println("Has ID3v1 tag?: " + (mp3file.hasId3v1Tag() ? "YES" : "NO"));
+            //System.out.println("Has ID3v2 tag?: " + (mp3file.hasId3v2Tag() ? "YES" : "NO"));
+            //System.out.println("Has custom tag?: " + (mp3file.hasCustomTag() ? "YES" : "NO"));
+            //System.out.println(mp3file.getId3v2Tag().getAlbumArtist());
+        } catch (IOException | UnsupportedTagException | InvalidDataException ex) {
+            Logger.getLogger(Frame.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        try {
+            mp3file.getId3v2Tag().setAlbum("cheese");
+            mp3file.save(file.getAbsolutePath().replace(".mp3", "1.mp3"));
+        } catch (IOException | NotSupportedException ex) {
+            Logger.getLogger(Frame.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    void showArtworkPopup(MouseEvent e) {
         JPopupMenu popup = new JPopupMenu();
         JMenuItem item;
         popup.add(item = new JMenuItem("Add"));
@@ -230,14 +317,47 @@ public class Frame extends javax.swing.JFrame {
 
         popup.show(e.getComponent(), e.getX(), e.getY());
     }
+    
+    void showRegularPopup(MouseEvent e) {
+        JPopupMenu popup = new JPopupMenu();
+        JMenuItem item;
+        popup.add(item = new JMenuItem("Remove from list"));
+        item.addActionListener(menuListener);
 
-    ActionListener menuListener = (ActionEvent event) -> {
-        if (event.getActionCommand().equals("Add")) {
+        popup.show(e.getComponent(), e.getX(), e.getY());
+    }
 
-        } else if (event.getActionCommand().equals("Remove")) {
-
+    public int findSong(String title, String artist, String album) {
+        for (int i = 0; i < songs.size(); i++) {
+            if(songs.get(i).getTitle().equals(title)
+                    && songs.get(i).getArtist().equals(artist)
+                    && songs.get(i).getAlbum().equals(album)) {
+                return i;
+            }
         }
-    };
+        return -1;
+    }
+    
+    public int getRow(String filename) {
+        for (int i = 0; i < table.getRowCount(); i++) {
+            if(model.getValueAt(i, 0).equals(filename)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    
+    public void removeUnchanged() {
+        for (int i = 0; i < songs.size(); i++) {
+            if(songs.get(i) == edited_songs.get(i)) {
+                updateConsole(songs.get(i).getTitle() + " wasn't modified. Removing.");
+                songs.remove(i);
+                edited_songs.remove(i);
+            } else {
+                updateConsole(songs.get(i).getTitle() + " was modified. Keeping.");
+            }
+        }
+    }
 
     /**
      * @param args the command line arguments
@@ -272,15 +392,15 @@ public class Frame extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JTextArea console;
+    private javax.swing.JScrollPane consoleSP;
     private javax.swing.JPanel container;
-    private javax.swing.JTextArea dragArea;
     private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
     private javax.swing.JMenu jMenu1;
     private javax.swing.JMenu jMenu2;
     private javax.swing.JMenuBar jMenuBar1;
-    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JButton saveButton;
     private javax.swing.JTable table;
     private javax.swing.JScrollPane tableSP;
     // End of variables declaration//GEN-END:variables
---
+++ src/moose/Song.java
@@ -0,0 +1,271 @@
+/**
+ *
+ */
+// package
+package moose;
+
+// imports
+import java.awt.Image;
+import java.io.File;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+
+// class Song
+public class Song {
+
+    //private String filepath;
+    //private String filename;
+    private File file;
+    private String title;
+    private String artist;
+    private String album;
+    private String albumartist;
+    private String genre;
+    private String track;
+    private String totaltracks;
+    private String disk;
+    private String totaldisks;
+    private Icon artwork;
+
+    public Song() {
+
+    }
+
+    public Song(File file, String title, String artist, String album, String albumartist, String genre, String track, String disk, byte[] artwork_bytes) {
+
+        // standard string stuff
+        this.file = file;
+        this.title = title;
+        this.artist = artist;
+        this.album = album;
+        this.albumartist = albumartist;
+        this.genre = genre;
+
+        // parsing track and disk for total tracks/disks
+        if (track != null && track.contains("/")) {
+            String[] t = track.split("/");
+            this.track = t[0];
+            this.totaltracks = t[1];
+        } else {
+            this.track = "0";
+            this.totaltracks = "0";
+        }
+        if (disk != null && disk.contains("/")) {
+            String[] d = disk.split("/");
+            this.disk = d[0];
+            this.totaldisks = d[1];
+        } else {
+            this.disk = "0";
+            this.totaldisks = "0";
+        }
+
+        try {
+            // getting the image from the byte array
+            ImageIcon icon = new ImageIcon(artwork_bytes);
+            Image img = icon.getImage();
+            Image img_scaled = img.getScaledInstance(100, 100, java.awt.Image.SCALE_SMOOTH);
+            this.artwork = new ImageIcon(img_scaled);
+        } catch (NullPointerException e) {
+
+        }
+    }
+
+    /**
+     * @return the full track string
+     */
+    public String getFullTrackString() {
+        if (track.equals("0") && totaltracks.equals("0")) {
+            return "";
+        } else {
+            return track + "/" + totaltracks;
+        }
+    }
+
+    /**
+     * @return the full disk string
+     */
+    public String getFullDiskString() {
+        if (disk.equals("0") && totaldisks.equals("0")) {
+            return "";
+        } else {
+            return disk + "/" + totaldisks;
+        }
+    }
+
+    /**
+     * @return the title
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * @param title the title to set
+     */
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    /**
+     * @return the artist
+     */
+    public String getArtist() {
+        return artist;
+    }
+
+    /**
+     * @param artist the artist to set
+     */
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    /**
+     * @return the album
+     */
+    public String getAlbum() {
+        return album;
+    }
+
+    /**
+     * @param album the album to set
+     */
+    public void setAlbum(String album) {
+        this.album = album;
+    }
+
+    /**
+     * @return the albumartist
+     */
+    public String getAlbumartist() {
+        return albumartist;
+    }
+
+    /**
+     * @param albumartist the albumartist to set
+     */
+    public void setAlbumartist(String albumartist) {
+        this.albumartist = albumartist;
+    }
+
+    /**
+     * @return the genre
+     */
+    public String getGenre() {
+        return genre;
+    }
+
+    /**
+     * @param genre the genre to set
+     */
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    /**
+     * @return the track
+     */
+    public String getTrack() {
+        return track;
+    }
+
+    /**
+     * @param track the track to set
+     */
+    public void setTrack(String track) {
+        this.track = track;
+    }
+
+    /**
+     * @return the totaltracks
+     */
+    public String getTotaltracks() {
+        return totaltracks;
+    }
+
+    /**
+     * @param totaltracks the totaltracks to set
+     */
+    public void setTotaltracks(String totaltracks) {
+        this.totaltracks = totaltracks;
+    }
+
+    /**
+     * @return the disk
+     */
+    public String getDisk() {
+        return disk;
+    }
+
+    /**
+     * @param disk the disk to set
+     */
+    public void setDisk(String disk) {
+        this.disk = disk;
+    }
+
+    /**
+     * @return the totaldisks
+     */
+    public String getTotaldisks() {
+        return totaldisks;
+    }
+
+    /**
+     * @param totaldisks the totaldisks to set
+     */
+    public void setTotaldisks(String totaldisks) {
+        this.totaldisks = totaldisks;
+    }
+
+    /**
+     * @return the artwork
+     */
+    public Icon getArtwork() {
+        return artwork;
+    }
+
+    /**
+     * @param artwork the artwork to set
+     */
+    public void setArtwork(Icon artwork) {
+        this.artwork = artwork;
+    }
+
+    /**
+     * @return the file
+     */
+    public File getFile() {
+        return file;
+    }
+
+    /**
+     * @param file the file to set
+     */
+    public void setFile(File file) {
+        this.file = file;
+    }
+
+    @Override
+    public String toString() {
+        //return "Song{" + "title=" + title + ", artist=" + artist + ", album=" + album + ", albumartist=" + albumartist + ", genre=" + genre + ", track=" + track + ", totaltracks=" + totaltracks + ", disk=" + disk + ", totaldisks=" + totaldisks + ", artwork=" + artwork + '}';
+        return this.file.getName();
+    }
+    
+    public boolean equals(Song s) {
+        if(s.getFile() == this.file
+                && s.getTitle().equals(this.title)
+                && s.getArtist().equals(this.artist)
+                && s.getAlbum().equals(this.album)
+                && s.getAlbumartist().equals(this.albumartist)
+                && s.getGenre().equals(this.genre)
+                && s.getFullTrackString().equals(this.getFullTrackString())
+                && s.getFullDiskString().equals(this.getFullDiskString())
+                && s.getArtwork() == this.getArtwork()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
