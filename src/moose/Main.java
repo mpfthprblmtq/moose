8fa65e14ca01e4415f54c7340cb97dab5ff53738
Pat Ripley <ripley307@gmail.com>
2018-12-20 10:58:26 -0600

    Update version

--- src/moose/Main.java
+++ src/moose/Main.java
@@ -19,7 +19,7 @@ import moose.views.SettingsFrame;
 public class Main {
     
     // version
-    public static String version = "1.1.1";
+    public static String version = "1.1.2";
     
     // create and instantiate the frames
     public static Frame frame;
