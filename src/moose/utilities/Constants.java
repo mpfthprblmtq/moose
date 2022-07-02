5f0b4527f44cc75bcfdd94751d112b18f3a209a5
mpfthprblmtq <ripley307@gmail.com>
2022-07-01 21:20:53 -0500

    fixed random bug with CD1/CD2 stuff and not being able to find the correct artist

--- src/moose/utilities/Constants.java
+++ src/moose/utilities/Constants.java
@@ -44,7 +44,7 @@ public class Constants {
     public static final String YEAR_ALBUM_REGEX = "\\[(?<Year>\\d{4})\\] (?<Album>.*)";
     public static final String TRACKNUM_ARTIST_TITLE_REGEX = "(?<TrackNumber>\\d{1,3}) (?<Artist>.*) - (?<Title>.*).mp3";
     public static final String TRACKNUM_ARTIST_TITLE_OPT_REGEX = "(\\d{1,2} )*(?<Artist>.*) - (?<Title>.*).mp3";
-    public static final String CD_FILEPATH_REGEX = ".*\\/(?<CDNumber>CD\\d+)\\.*";
+    public static final String CD_FILEPATH_REGEX = ".*\\/(?<CDNumber>CD\\d+)\\/.*";
     public static final String YOUTUBE_FILENAME_REGEX = "\\w+\\.\\w{2} - (?<FileName>.*)-.{11}-\\d{3}k-\\d{13}.mp3";
     public static final String[] FILENAME_STRINGS_TO_REMOVE = new String[] {
             "LYRIC VIDEO",
